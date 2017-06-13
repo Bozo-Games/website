502f08484c9e81a024501f5af6b2a12cab5aa2b9
BillDNA <BillDNA@users.noreply.github.com>
2017-06-13 13:05:05 -0700

    now with extra script loading not just listing

--- routes/experiment.js
+++ routes/experiment.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var experiments =  [
-    {
-        name:'snake',
-        about:'a remake of the classic snake.',
-        extraScripts: []
-    },{
-        name:'find-my-tv',
-        about:'How do we have a TV load our pages?',
-        extraScripts: [
-            "http://www.gstatic.com/cast/sdk/libs/receiver/2.0.0/cast_receiver.js"
-        ]
-    }
-];
+var experiments = {
+        snake: {
+            name: 'snake',
+            about: 'a remake of the classic snake.',
+            extraScripts: []
+        },
+        "find-my-tv": {
+            name: 'find-my-tv',
+            about: 'How do we have a TV load our pages?',
+            extraScripts: [
+                "http://www.gstatic.com/cast/sdk/libs/receiver/2.0.0/cast_receiver.js"
+            ]
+        }
+    };
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log(experiments);
     res.render('experiments', {
         title: 'Some things to try',
         experiments:experiments
@@ -22,6 +24,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:name', function(req,res, next) {
+
     res.render('templates/experiment', {
         name:req.params.name,
         extraScripts:experiments[req.params.name].extraScripts

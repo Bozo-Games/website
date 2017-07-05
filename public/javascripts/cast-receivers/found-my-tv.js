011e8e74b407f2b63a420d40488fe36269c1a8ee
Bill Decker <bill.s.decker@gmail.com>
2017-07-05 13:52:11 -0700

    testign

--- public/javascripts/cast-receivers/found-my-tv.js
+++ public/javascripts/cast-receivers/found-my-tv.js
@@ -4,19 +4,14 @@ function setup() {
     //loadColors
     settings.color.load();
     createCanvas(windowWidth,windowHeight);
-    background(settings.color.black);
-
-    window.mediaElement = document.getElementById('media');
-    window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
-    window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-    window.castReceiverManager.start();
+    background(settings.color.red);
 
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 function draw() {
-    background(settings.color.black);
+    background(settings.color.red);
 }
 
 function keyReleased() {
